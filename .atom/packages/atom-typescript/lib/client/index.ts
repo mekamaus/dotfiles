47bf81d189dce1fb20a2720ce887446897cf1d9e
Sam Adams <samadams532@gmail.com>
2019-11-12 12:11:45 -0800

    fix gitignore again

--- .atom/packages/atom-typescript/lib/client/index.ts
+++
@@ -1,7 +0,0 @@
-export {ClientResolver, resolveBinary} from "./clientResolver"
-
-import {TypescriptServiceClient} from "./client"
-
-export {TypescriptServiceClient as TSClient}
-
-export type GetClientFunction = (filePath: string) => Promise<TypescriptServiceClient>
