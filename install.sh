90600d123ae8422c6658b631f7f9390dfdf97ec7
Sam Adams <samwise@uber.com>
2020-08-04 01:09:25 -0500

    vundle

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
+# config
 gh="https://github.com"
 gh_raw="https://raw.github.com"
 
-# zsh themes, plugins
 zsh_custom="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 zsh_themes="$zsh_custom/themes"
 zsh_plugins="$zsh_custom/plugins"
@@ -14,12 +14,20 @@ zsh_autosuggestions_git="$gh/zsh-users/zsh-autosuggestions"
 zsh_autosuggestions_dest="$zsh_plugins/zsh-autosuggestions"
 zsh_sh_git="$gh/zsh-users/zsh-syntax-highlighting.git"
 zsh_sh_dest="$zsh_plugins/zsh-syntax-highlighting"
+vundle_git="$gh/VundleVim/Vundle.vim.git"
+vundle_dest="~/.vim/bundle/Vundle.vim"
+
+# zsh themes, plugins
 
 sh -c "$(curl -fsSL "$omz_install")" 2> /dev/null
 git clone --depth=1 "$p10k_git" "$p10k_dest" 2> /dev/null
 git clone "$zsh_autosuggestions_git" "$zsh_autosuggestions_dest" 2> /dev/null
 git clone "$zsh_sh_git" "$zsh_sh_dest" 2> /dev/null
 
+# vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# symlinks
 base_dir="$(dirname "$0")"
 rm ~/.inputrc; ln -s "$base_dir/.inputrc" ~/.inputrc 2> /dev/null
 rm ~/.p10k.zsh; ln -s "$base_dir/.p10k.zsh" ~/.p10k.zsh 2> /dev/null
