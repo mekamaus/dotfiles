4351fd101263ec918b0ffe252aa005725927b74e
Sam Adams <samwise@uber.com>
2020-08-04 00:37:39 -0500

    fail silently

--- install.sh
+++ install.sh
@@ -11,20 +11,20 @@ omz_install="$gh_raw/ohmyzsh/ohmyzsh/master/tools/install.sh"
 p10k_git="$gh/romkatv/powerlevel10k.git"
 p10k_dest="$zsh_themes/powerlevel10k"
 zsh_autosuggestions_git="$gh/zsh-users/zsh-autosuggestions"
-zsh_autosuggestions_dest="$zsh_plugins/zsh_autosuggestions"
+zsh_autosuggestions_dest="$zsh_plugins/zsh-autosuggestions"
 zsh_sh_git="$gh/zsh-users/zsh-syntax-highlighting.git"
 zsh_sh_dest="$zsh_plugins/zsh-syntax-highlighting"
 
-sh -c "$(curl -fsSL "$omz_install")"
-git clone --depth=1 "$p10k_git" "$p10k_dest"
-git clone "$zsh_autosuggestions_git" "$zsh_autosuggestions_dest"
-git clone "$zsh_sh_git" "$zsh_sh_dest"
+sh -c "$(curl -fsSL "$omz_install")" 2> /dev/null
+git clone --depth=1 "$p10k_git" "$p10k_dest" 2> /dev/null
+git clone "$zsh_autosuggestions_git" "$zsh_autosuggestions_dest" 2> /dev/null
+git clone "$zsh_sh_git" "$zsh_sh_dest" 2> /dev/null
 
 base_dir="$(dirname "$0")"
-ln -s "$base_dir/.inputrc" ~/.inputrc
-ln -s "$base_dir/.p10k.zsh" ~/.p10k.zsh
-ln -s "$base_dir/.tmux.conf" ~/.tmux.conf
-ln -s "$base_dir/.tmux.conf.local" ~/.tmux.conf.local
-ln -s "$base_dir/.vimrc" ~/.vimrc
-ln -s "$base_dir/.zshrc" ~/.zshrc
+ln -s "$base_dir/.inputrc" ~/.inputrc 2> /dev/null
+ln -s "$base_dir/.p10k.zsh" ~/.p10k.zsh 2> /dev/null
+ln -s "$base_dir/.tmux.conf" ~/.tmux.conf 2> /dev/null
+ln -s "$base_dir/.tmux.conf.local" ~/.tmux.conf.local 2> /dev/null
+ln -s "$base_dir/.vimrc" ~/.vimrc 2> /dev/null
+ln -s "$base_dir/.zshrc" ~/.zshrc 2> /dev/null
 
